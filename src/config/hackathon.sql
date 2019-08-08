a0284b63fe34f3438bf5728876f65399de8d0a99
ubuntu <ubuntu@test.de>
2019-08-08 11:30:16 +0200

    added table + column

--- src/config/hackathon.sql
+++ src/config/hackathon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Aug 08, 2019 at 08:06 AM
+-- Generation Time: Aug 08, 2019 at 09:28 AM
 -- Server version: 10.4.7-MariaDB-1:10.4.7+maria~bionic
 -- PHP Version: 7.2.19
 
@@ -120,6 +120,17 @@ CREATE TABLE `product_category` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `product_user`
+--
+
+CREATE TABLE `product_user` (
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -133,15 +144,16 @@ CREATE TABLE `user` (
   `street` varchar(255) CHARACTER SET utf8 NOT NULL,
   `zip` varchar(50) CHARACTER SET utf8 NOT NULL,
   `city` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `country` varchar(100) NOT NULL
+  `country` varchar(100) NOT NULL,
+  `last_log_in` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `login`, `firstname`, `lastname`, `email`, `password`, `street`, `zip`, `city`, `country`) VALUES
-(1, '', 'First', 'User', 'first@mail.de', '*DB006D8D64953C5663EA467BB08DF59C16B33FCE', 'the street', '98765', 'city', 'Germany');
+INSERT INTO `user` (`user_id`, `login`, `firstname`, `lastname`, `email`, `password`, `street`, `zip`, `city`, `country`, `last_log_in`) VALUES
+(1, '', 'First', 'User', 'first@mail.de', '*DB006D8D64953C5663EA467BB08DF59C16B33FCE', 'the street', '98765', 'city', 'Germany', 0);
 
 --
 -- Indexes for dumped tables
@@ -198,6 +210,12 @@ ALTER TABLE `product_category`
   ADD KEY `product_category` (`product_id`,`category_id`);
 
 --
+-- Indexes for table `product_user`
+--
+ALTER TABLE `product_user`
+  ADD KEY `product_user` (`product_id`,`user_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -241,6 +259,12 @@ ALTER TABLE `order`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product_user`
+--
+ALTER TABLE `product_user`
   MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
